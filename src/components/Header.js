6218d3f44696736bb3c2fbb16f66587a0f05449e
Manas-Pathak <pathakmanas111@gmail.com>
2024-06-14 01:39:15 +0530

    some changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,10 +14,10 @@ function Header() {
       <div id="header__bar" style = {{transform: showHeaderBar? `translateY(120px)`: `translateY(0px)`}}>
         <h3 className="header__bar__title">Frontend Developer</h3>
         <div className="header__bar__icons">
-          <a href="https://www.facebook.com/dasharath.dasharath.1694/" target='_blank'><img src="https://img.icons8.com/ios/50/000000/facebook--v1.png"/></a>
-          <a href="https://www.instagram.com/dasharath_116/" target='_blank'><img src="https://img.icons8.com/ios/50/000000/instagram-new--v1.png"/></a>
-          <a href="https://github.com/Dasharath9920" target='_blank'><img src="https://img.icons8.com/ios/50/000000/github--v1.png"/></a>
-          <a href="https://www.linkedin.com/in/dyavari-dasharath-b767021b3/" target='_blank'><img src="https://img.icons8.com/ios/50/000000/linkedin.png"/></a>
+         
+          <a href="https://www.instagram.com/manas_pathak_1/" target='_blank'><img src="https://img.icons8.com/ios/50/000000/instagram-new--v1.png"/></a>
+          <a href="https://github.com/Manas-Pathak" target='_blank'><img src="https://img.icons8.com/ios/50/000000/github--v1.png"/></a>
+          <a href="https://www.linkedin.com/in/manaspathak/" target='_blank'><img src="https://img.icons8.com/ios/50/000000/linkedin.png"/></a>
         </div>
       </div>
 
